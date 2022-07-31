38c6f625bf193ded8356fca5cf6a61e1f399b76a
CANHDINH31 <88915565+CANHDINH31@users.noreply.github.com>
2022-07-31 21:52:57 +0700

    Fix cors

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ const connect = () => {
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
